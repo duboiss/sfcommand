806c8f2101e8ce16d7f0ea0da3ea0bb15c21e705
Steven DUBOIS <git@duboiss.fr>
2021-07-02 11:27:11 +0200

    DefaultCommand with PHP 8

--- src/Command/DefaultCommand.php
+++ src/Command/DefaultCommand.php
@@ -3,23 +3,19 @@
 namespace App\Command;
 
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'app:default',
+    description: 'wrapper',
+    hidden: true
+)]
 class DefaultCommand extends Command
 {
-    protected static $defaultName = 'app:default';
-
-    protected function configure(): void
-    {
-        $this
-            ->setDescription('wrapper')
-            ->setHidden(true)
-        ;
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output): ?int
     {
         /** @var Application $application */
